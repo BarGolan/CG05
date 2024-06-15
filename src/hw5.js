6e6c92ed74f27db623657059a9db5a1c5e1bca3c
Zilberr <alonzilberr@gmail.com>
2024-06-15 14:27:44 +0300

    Black Ball

--- src/hw5.js
+++ src/hw5.js
@@ -275,7 +275,7 @@ function generateGoal() {
 
 function generateBall() {
   let ballGeometry = new THREE.SphereGeometry(0.125, 32, 32);
-  let ballMaterial = new THREE.MeshBasicMaterial({ color: 0xffaa11 });
+  let ballMaterial = new THREE.MeshBasicMaterial({ color: 0x000000 });
   let ball = new THREE.Mesh(ballGeometry, ballMaterial);
   translateObject(ball, 0, 1.5, 2.5);
 
